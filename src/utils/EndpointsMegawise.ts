c4a42aa187302e64d907fe0162cda3a8b18c6fe0
ruiyi.jiang <ruiyi.jiang@zilliz.com>
2020-04-08 09:28:58 +0800

    fix url

--- src/utils/EndpointsMegawise.ts
+++ src/utils/EndpointsMegawise.ts
@@ -3,11 +3,10 @@ declare global {
     _env_: any;
   }
 }
-// let endpoint = `http://127.0.0.1:5555`;
 let endpoint = `http://192.168.1.169:9000`;
-// if (window._env_ && window._env_.API_URL) {
-//   endpoint = window._env_.API_URL;
-// }
+if (window._env_ && window._env_.API_URL) {
+  endpoint = window._env_.API_URL;
+}
 export const Query = `${endpoint}/query`;
 export const POST_TABLES_DETAIL = `${endpoint}/db/tables/detail`;
 export const GET_TABLE_LIST = `${endpoint}/db/tables`;
