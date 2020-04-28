69e47d99a4a254ea2811ba98b6e600793d4926d4
AzAz <az15921869329@gmail.com>
2020-04-28 11:31:36 +0800

    add covid_china

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -7,7 +7,8 @@ import shanghai_car from './shanghai_car.json';
 import shenzhen_car from './shenzhen_car.json';
 import nyc_taxi_arctern from './nyc_taxi_point_arctern.json';
 import covid_arctern from './covid_arctern.json';
+import covid_china from './covid_china.json';
 
 export const dashboards = [nyc_taxi, nyc_taxi_heat, nyc_taxi_point, shanghai_car, shenzhen_car];
 
-export const dashboards_arctern = [nyc_taxi_arctern, covid_arctern];
+export const dashboards_arctern = [nyc_taxi_arctern, covid_china, covid_arctern];
